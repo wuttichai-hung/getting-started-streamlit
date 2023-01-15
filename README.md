d0058770d9d20adc68e4ed2b4f1645bb19452f36
wuttichaihung <wuttichaihung@gmail.com>
2023-01-15 22:28:29 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # getting-started-streamlit
+
+# requirements.txt
+- pip install streamlit
+
+`streamlit run main.py`
---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+
+with st.echo():
+    st.title("Getting Stated Streamlit")
+    st.write("This is introduction to streamlit")
+
+    st.markdown("## Code")
+    code = '''
+    def hello():
+        print("Hello, Streamlit!")
+    '''
+
+    show_btn = st.button("Show code!")
+    if show_btn:
+        st.code(code, language='python')
+
+
+    cols = st.columns(2)
+    with cols[0]:
+        age_inp = st.number_input("Input your age")
+        st.markdown(f"Your age is {age_inp}")
+
+
+    # st.markdown("# NLP Task")
+    with cols[1]:
+        text_inp = st.text_input("Input your text")
+        word_tokenize = "|".join(text_inp.split())
+        st.markdown(f"{word_tokenize}")
+
+
+    df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 60, 90]
+    })
+    st.dataframe(df)
+    show_chart_btn = st.button("Show Chart!!")
+    if show_chart_btn:
+        st.line_chart(df, x='first column', y='second column')
